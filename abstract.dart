2c187265bedd1c9c9b9f0a99ce128d6262172376
futurekelly <futurekelly360@gmail.com>
2025-12-29 19:34:37 +0300

    adding some override class

--- abstract.dart
+++ abstract.dart
@@ -1,1 +1,23 @@
 //this is the comment
+abstract class Animal {
+  void makeSound();
+}
+class Dog extends Animal {
+  @override
+  void makeSound() {
+    print('Woof!');
+  }
+}
+class Cat extends Animal {
+  @override
+  void makeSound() {
+    print('Meow!');
+  }
+}
+void main() {
+  Animal myDog = Dog();
+  Animal myCat = Cat();
+
+  myDog.makeSound(); // Output: Woof!
+  myCat.makeSound(); // Output: Meow!
+}
